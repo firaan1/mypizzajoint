b319dc6b1a1452ed5947cbb2cca203b56e606ed9
firaan1 <firaan1@gmail.com>
2018-08-13 23:29:25 +0200

    trying out login

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "orders/login.html", {"message" : None})
+    context = {
+    "user" : request.user
+    }
     return HttpResponse("Project 3: TODO")
