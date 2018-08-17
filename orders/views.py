b5278558be8693eb52e594eb63cbb88ec07ebe0b
firaan1 <firaan1@gmail.com>
2018-08-17 05:07:14 +0200

    working on menu selection

--- orders/views.py
+++ orders/views.py
@@ -5,21 +5,48 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
+from django.core.serializers import serialize
+import json
+
+from .models import *
+
 # check email function
 import re
 def check_email(email):
   return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
-
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return render(request, "orders/login.html", {"message" : None})
     context = {
-    "user" : request.user
+        "pizzas" : PizzaRate.objects.all()
     }
     # return HttpResponse("Project 3: TODO")
-    return render(request, "orders/index.html") 
+    return render(request, "orders/index.html", context)
+
+@login_required(login_url='/login')
+def order(request):
+    # pizzatype = list(set([(x.pizzatype_id, str(x.pizzatype)) for x in PizzaRate.objects.all()]))
+    # pizzasize = list(set([(x.pizzasize_id, str(x.pizzasize)) for x in PizzaRate.objects.all()]))
+    # toppingtype = list(set([(x.toppingtype_id, str(x.toppingtype)) for x in PizzaRate.objects.all()]))
+    # toppingchoice = list([(t.pk,t.toppingchoice) for t in ToppingChoice.objects.all()])
+    pizzatype,pizzasize,toppingtype = {},{},{}
+    for p in PizzaRate.objects.all():
+        pizzatype[p.pizzatype_id] = str(p.pizzatype)
+        pizzasize[p.pizzasize_id] = str(p.pizzasize)
+        toppingtype[p.toppingtype_id] = str(p.toppingtype)
+    toppingchoice = {}
+    for t in ToppingChoice.objects.all():
+        toppingchoice[t.pk] = str(t)
+    context = {
+    "pizzatype": json.dumps(pizzatype),
+    "pizzasize": json.dumps(pizzasize),
+    "toppingtype": json.dumps(toppingtype),
+    "toppingchoice": json.dumps(toppingchoice),
+    "pizzarate": PizzaRate.objects.all()
+    }
+    return render(request, "orders/order.html", context)
 
 def login_view(request):
     if request.method == "GET":
